2d6ab4516a15f658d503fa0ebba879b887b238a5
SuhailArshad1122 <suhail_a@hotmail.com>
2020-11-06 01:00:32 +0500

    my 6th commit

--- src/pages/templates/products.tsx
+++ src/pages/templates/products.tsx
@@ -6,7 +6,7 @@ export default function Products({ pageContext }) {
     console.log(pageContext);
     const {itemDetails} = pageContext
     return <Layout>         
-        <h1>{itemDetails.title}</h1>
+        {/* <h1>{itemDetails.title}</h1> */}
         <p> { documentToReactComponents(itemDetails.description.json) }</p>
         </Layout>
 }
