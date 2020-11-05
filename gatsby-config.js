11ec9bdcb4f8f5a445473351d23550f78fc548e4
SuhailArshad1122 <suhail_a@hotmail.com>
2020-11-05 23:38:09 +0500

    3rd commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,13 +16,13 @@ module.exports = {
         },
       },
 
-      {
-        resolve: `gatsby-source-contentful`,
-        options: {
-          spaceId: process.env.CONTENTFUL_SPACE_ID,
-          accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        },
-      },
+      // {
+      //   resolve: `gatsby-source-contentful`,
+      //   options: {
+      //     spaceId: process.env.CONTENTFUL_SPACE_ID,
+      //     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      //   },
+      // },
 
 
   ],
