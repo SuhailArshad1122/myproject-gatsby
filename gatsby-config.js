41f192677e1f05d434c1597fb63e47c9569ff369
SuhailArshad1122 <suhail_a@hotmail.com>
2020-11-06 12:44:14 +0500

    7rd commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,12 +10,12 @@ module.exports = {
       {
         resolve: `gatsby-source-contentful`,
         options: {
-          //spaceId: process.env.CONTENTFUL_SPACE_ID,
-          // spaceId: "n732iqjl3sun",
+          spaceId: process.env.CONTENTFUL_SPACE_ID,
+          // spaceId: "",
           
           // Learn about environment variables: https://gatsby.dev/env-vars
-          //accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-          // accessToken: "whxejhBnB82_OrHTF3riVmmHSmtXSUsYJZhUSvoqqX8",
+          accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+          // accessToken: "",
           
         },
       },
